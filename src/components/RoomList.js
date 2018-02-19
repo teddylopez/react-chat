44fe8dc8c8949870bb0a167a952acddd6be4ab66
teddylopez <tedmlopez@gmail.com>
2018-02-19 15:49:21 -0500

    Styling.

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,10 +5,22 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
       this.state = {
+        name: '',
         rooms: []
       };
     this.roomsRef = this.props.firebase.database().ref('rooms');
-    console.log(this.roomsRef);
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push({ name: this.state.name });
+    this.setState({ name: '' });
   }
 
   componentDidMount() {
@@ -20,16 +32,23 @@ class RoomList extends Component {
    }
 
   render() {
-    const roomList = this.state.rooms.map((room) =>
-      <li key={room.key}>{room.name}</li>
-    );
-    return(
-      <div className="chat-list">
-        <h3>Rooms</h3>
-        <ul className="room-names">{roomList}</ul>
-      </div>
-    );
-  }
+    const roomForm = (
+      <form onSubmit={this.createRoom}>
+        <input type="text" value={this.state.name} placeholder="Room Name" onChange={this.handleChange}/>
+        <input type="submit" value="Create" id="room-submit" />
+        </form>
+      );
+
+      const roomList = this.state.rooms.map((room) =>
+        <li key={room.key}>{room.name}</li>
+      );
+      return(
+        <div className="chat-list">
+          <div>{roomForm}</div>
+          <ul className="room-names">{roomList}</ul>
+        </div>
+      );
+    }
 }
 
 export default RoomList
