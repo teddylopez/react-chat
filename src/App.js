2e9b8292540bd7b1f2b0bb7dc145618986dbff46
teddylopez <tedmlopez@gmail.com>
2018-02-18 21:19:11 -0500

    Troubleshooting Firebase.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import AppHeader from './components/AppHeader';
 import Landing from './components/Landing';
 import SideBar from './components/SideBar';
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBbaQg_RBFBD078qRilJHs_GmwTuNceaDQ",
+  authDomain: "react-chat-edd54.firebaseapp.com",
+  databaseURL: "https://react-chat-edd54.firebaseio.com",
+  projectId: "react-chat-edd54",
+  storageBucket: "react-chat-edd54.appspot.com",
+  messagingSenderId: "419133942033"
+};
+firebase.initializeApp(config);
+
 class App extends Component {
   render() {
     return (
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 import './../styles/roomlist.css';
 
 class RoomList extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        rooms: []
+      };
+    //this.roomsRef = this.props.firebase.database().ref('rooms');
+    console.log(this.props.firebase.database);
+  }
 
-  this.state = {
-    rooms: []
-  };
-  
   render() {
     return (
       <div id="rooms">
